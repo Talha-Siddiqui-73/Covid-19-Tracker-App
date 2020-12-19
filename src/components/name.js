4a009d5e1907da2446631d1a02e3391c7d98546a
Talha Siddiqui <72685596+Talha-Siddiqui-73@users.noreply.github.com>
2020-12-19 22:47:06 +0500

    Covid-19 Tracker App Using React JS
    Covid-19 Tracker App Using React JS (Material UI, Chart js, Hooks)

---
+++ src/components/name.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './name.css';
+
+function Name() {
+  return (
+    <div className="App">
+     <h1> Talha Siddiqui </h1>
+    </div>
+  );
+}
+
+export default Name;
